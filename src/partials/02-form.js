d02855939efe1a19edd26eb636bc5b01e9554108
GrzegorzKorpa <grzegorz.korpa@post.pl>
2024-08-27 16:20:11 +0200

    index.html

--- src/partials/02-form.js
+++ src/partials/02-form.js
@@ -2,48 +2,29 @@ const form = document.querySelector(`.feedback-form`);
 
 form.addEventListener('submit', submitDone);
 
-
-
 const email = document.querySelector(`.email`);
 
 const message = document.querySelector(`.message`);
 
-
-
 const saveFormData =
-
   JSON.parse(localStorage.getItem('feedback-form-state')) || {};
 
-
-
 form.addEventListener('input', evt => {
-
   if (evt.target === email) {
-
     saveFormData.email = evt.target.value.trim();
-
     localStorage.setItem('feedback-form-state', JSON.stringify(saveFormData));
-
   } else if (evt.target === message) {
-
     saveFormData.message = evt.target.value.trim();
-
     localStorage.setItem('feedback-form-state', JSON.stringify(saveFormData));
-
   }
-
 });
-
-
 
 function submitDone(event) {
 
   event.preventDefault();
 
   if (email.value === '' || message.value === '') {
-
     return alert('All form fields must be filled in');
-
   }
 
   console.log('Submit done!');
@@ -55,5 +36,4 @@ function submitDone(event) {
   localStorage.removeItem('feedback-form-state');
 
   alert(`Good job!\nSubmit done!`);
-
 }
